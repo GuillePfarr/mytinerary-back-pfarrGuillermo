d5653bf5f811502d4d40b42227935551e98e2f01
GuillePfarr <guillepfarr@hotmail.com>
2023-08-29 18:24:54 -0300

    Recargando ciudades en la DB

--- cities.js
+++ cities.js
@@ -1,21 +1,190 @@
 const cities = [
 
-{
- name: 'Zermatalons',
-description: 'Sart',
-currency: 'CanDol',
-lang: 'FrenchEnglish',
-time: 'GTMp5',
-},
+    {
+        name: "Salzburg",
+        country: "Austria",
+        description: "Center-European",
+        currency: "Euro",
+        lang: "German",
+        time: "GMT+2",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Salzburg_panorama.jpg/1280px-Salzburg_panorama.jpg",
+    }
+
+    ,
+
+    {
+       name: "Barcelona",
+        country: "Spain",
+        description: "Mediterranean",
+        currency: "Euro",
+        lang: "Spanish",
+        time: "GMT+2",
+        image: "https://www.wendyperrin.com/wp-content/uploads/2017/12/shutterstock_388289746.jpg",
+    }
+
+,
 
 {
- name: 'MongoDB',
-description: 't4oMDpmkNf10L9Qu',
-currency: 'Crypto',
-lang: 'universal',
-time: 'infinit',
-}
+       name: "Athens",
+        country: "Greek",
+        description: "Mediterranean",
+        currency: "Euro",
+        lang: "Greek",
+        time: "GMT+3",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Attica_06-13_Athens_25_Olympian_Zeus_Temple.jpg/800px-Attica_06-13_Athens_25_Olympian_Zeus_Temple.jpg",
+    }
+,
 
+{
+       name: "Rome",
+        country: "Italy",
+        description: "Mediterranean",
+        currency: "Euro",
+        lang: "",
+        time: "GMT+2",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Colosseum_in_Rome-April_2007-1-_copie_2B.jpg/1024px-Colosseum_in_Rome-April_2007-1-_copie_2B.jpg",
+    }
+
+,
+
+{
+       name: "Ushuahia",
+        country: "Argentina",
+        description: "Austral",
+        currency: "Peso",
+        lang: "Spanish",
+        time: "GTM-3",
+        image: "https://miro.medium.com/v2/resize:fit:1400/1*I4waGmtnO9fi9ij3yyAvxA.jpeg",
+    }
+
+,
+
+{
+       name: "Barcelona",
+        country: "Spain",
+        description: "Mediterranean",
+        currency: "Euro",
+        lang: "Spanish",
+        time: "GMT+2",
+        image: "https://www.wendyperrin.com/wp-content/uploads/2017/12/shutterstock_388289746.jpg",
+    }
+,
+
+{
+       name: "Barcelona",
+        country: "Spain",
+        description: "Mediterranean",
+        currency: "Euro",
+        lang: "Spanish",
+        time: "GMT+2",
+        image: "https://www.wendyperrin.com/wp-content/uploads/2017/12/shutterstock_388289746.jpg",
+    }
+
+,
+
+{
+       name: "Barcelona",
+        country: "Spain",
+        description: "Mediterranean",
+        currency: "Euro",
+        lang: "Spanish",
+        time: "GMT+2",
+        image: "https://www.wendyperrin.com/wp-content/uploads/2017/12/shutterstock_388289746.jpg",
+    }
+
+,
+
+    {
+        name: "Barcelona",
+        country: "Spain",
+        description: "Mediterranean",
+        currency: "Euro",
+        lang: "Spanish",
+        time: "GMT+2",
+        image: "https://www.wendyperrin.com/wp-content/uploads/2017/12/shutterstock_388289746.jpg",
+    }
+
+    ,
+
+    {
+       name: "Barcelona",
+        country: "Spain",
+        description: "Mediterranean",
+        currency: "Euro",
+        lang: "Spanish",
+        time: "GMT+2",
+        image: "https://www.wendyperrin.com/wp-content/uploads/2017/12/shutterstock_388289746.jpg",
+    }
+
+,
+
+{
+       name: "Barcelona",
+        country: "Spain",
+        description: "Mediterranean",
+        currency: "Euro",
+        lang: "Spanish",
+        time: "GMT+2",
+        image: "https://www.wendyperrin.com/wp-content/uploads/2017/12/shutterstock_388289746.jpg",
+    }
+,
+
+{
+       name: "Barcelona",
+        country: "Spain",
+        description: "Mediterranean",
+        currency: "Euro",
+        lang: "Spanish",
+        time: "GMT+2",
+        image: "https://www.wendyperrin.com/wp-content/uploads/2017/12/shutterstock_388289746.jpg",
+    }
+
+,
+
+{
+       name: "Barcelona",
+        country: "Spain",
+        description: "Mediterranean",
+        currency: "Euro",
+        lang: "Spanish",
+        time: "GMT+2",
+        image: "https://www.wendyperrin.com/wp-content/uploads/2017/12/shutterstock_388289746.jpg",
+    }
+
+,
+
+{
+       name: "Barcelona",
+        country: "Spain",
+        description: "Mediterranean",
+        currency: "Euro",
+        lang: "Spanish",
+        time: "GMT+2",
+        image: "https://www.wendyperrin.com/wp-content/uploads/2017/12/shutterstock_388289746.jpg",
+    }
+,
+
+{
+       name: "Barcelona",
+        country: "Spain",
+        description: "Mediterranean",
+        currency: "Euro",
+        lang: "Spanish",
+        time: "GMT+2",
+        image: "https://www.wendyperrin.com/wp-content/uploads/2017/12/shutterstock_388289746.jpg",
+    }
+
+,
+
+{
+       name: "Barcelona",
+        country: "Spain",
+        description: "Mediterranean",
+        currency: "Euro",
+        lang: "Spanish",
+        time: "GMT+2",
+        image: "https://www.wendyperrin.com/wp-content/uploads/2017/12/shutterstock_388289746.jpg",
+    }
 ];
 
 
--- config/Models/City.js
+++ config/Models/City.js
@@ -1,16 +1,18 @@
 import { Schema, model } from "mongoose";
 
 const citySchema = Schema({
- name: {type: String, required: true},
-description: {type: String, required: true},
-currency: {type: String, required: true},
-lang: {type: String, required: true},
-time: {type: String, required: true},
+    name: { type: String, required: true },
+    country: { type: String, required: true },
+    description: { type: String, required: true },
+    currency: { type: String, required: true },
+    lang: { type: String, required: true },
+    time: { type: String, required: true },
+    image: { type: String, required: false },
 },
 
-{
-timestamps: true
-}
+    {
+        timestamps: true
+    }
 )
 const City = model('cities', citySchema)
 
