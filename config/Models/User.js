b8248da6087d9c3e5646e731269dbd3be4bf8676
GuillePfarr <guillepfarr@hotmail.com>
2023-09-12 19:55:45 -0300

    signup y signin funcionando

--- config/Models/User.js
+++ config/Models/User.js
@@ -4,11 +4,11 @@ const userSchema = new mongoose.Schema({
 name:  {type:String, required: true},
 last_name: {type:String, required: true},
 password:  {type:String, required: true},
-email: {type:String, required: true},
+email: {type:String, required: true, unique: true},
 image:  {type:String, required: true},
 country: {type:String},
 })
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('user', userSchema)
 
 export default User
 
