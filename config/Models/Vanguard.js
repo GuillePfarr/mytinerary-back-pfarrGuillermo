3d83a45a75c4182e050ad3d0b5798cc8d7be6f75
GuillePfarr <guillepfarr@hotmail.com>
2024-01-14 09:48:08 -0300

    Vanguard nidel created also vanguardgui branch

---
+++ config/Models/Vanguard.js
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+
+const citySchema = Schema({
+    tempInt1: { type: Number, required: true },
+    tempInt2: { type: Number, required: true },
+    tempExt: { type: Number, required: true },
+    humidityInt1: { type: Number, required: true },
+    humidityInt2: { type: Number, required: true },
+    motor1: { type: String, required: true },
+    motor2: { type: Number, required: true },
+    upperLimit1: { type: Number, required: true },
+    upperLimit2: { type: Number, required: true },
+    lowerLimit1: { type: Number, required: true },
+    lowerLimit2: { type: Number, required: true },
+    time: { type: time, required: true },
+    date: { type: Date, required: true }, 
+},
+
+    {
+        timestamps: true
+    }
+)
+const City = model('vanguards', citySchema)
+
+export default Vanguard
