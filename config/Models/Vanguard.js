850bb82146e2945ae5570516d5c97359364ad530
GuillePfarr <guillepfarr@hotmail.com>
2024-01-14 11:37:23 -0300

    Modified

--- config/Models/Vanguard.js
+++ config/Models/Vanguard.js
@@ -1,19 +1,19 @@
 import { Schema, model } from "mongoose";
 
 const vanguardSchema = new mongoose.Schema({
-    tempInt1: { type: Number, required: true },
-    tempInt2: { type: Number, required: true },
-    tempExt: { type: Number, required: true },
-    humidityInt1: { type: Number, required: true },
-    humidityInt2: { type: Number, required: true },
-    motor1: { type: String, required: true },
-    motor2: { type: Number, required: true },
-    upperLimit1: { type: Number, required: true },
-    upperLimit2: { type: Number, required: true },
-    lowerLimit1: { type: Number, required: true },
-    lowerLimit2: { type: Number, required: true },
-    time: { type: time, required: true },
-    date: { type: Date, required: true }, 
+    tempInt1: { type: Number, required: true }
+    // tempInt2: { type: Number, required: true },
+    // tempExt: { type: Number, required: true },
+    // humidityInt1: { type: Number, required: true },
+    // humidityInt2: { type: Number, required: true },
+    // motor1: { type: String, required: true },
+    // motor2: { type: Number, required: true },
+    // upperLimit1: { type: Number, required: true },
+    // upperLimit2: { type: Number, required: true },
+    // lowerLimit1: { type: Number, required: true },
+    // lowerLimit2: { type: Number, required: true },
+    // time: { type: time, required: true },
+    // date: { type: Date, required: true }, 
 },
 
     {
