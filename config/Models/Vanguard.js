838d5031d13e310066a2c7b9036557b06f3a0164
GuillePfarr <guillepfarr@hotmail.com>
2024-03-24 05:55:43 -0300

    New Vanguard Model

--- config/Models/Vanguard.js
+++ config/Models/Vanguard.js
@@ -2,19 +2,24 @@ import { Schema, model } from "mongoose";
 
 const vanguardSchema =   Schema({
     tempInt1: { type: Number},
-    tempInt2: { type: Number },
+    tempExt1: { type: Number },
     humyInt1: { type: Number },
-    maxTempInt1Reg: { type: Number },
-    minTempInt1Reg: { type: Number },
-    // maxTempDateHour: { type: Date, required: true },
-    // minTempDateHour: { type: Date, required: true },
-    // motor2: { type: Number, required: true },
-    // upperLimit1: { type: Number, required: true },
-    // upperLimit2: { type: Number, required: true },
-    // lowerLimit1: { type: Number, required: true },
-    // lowerLimit2: { type: Number, required: true },
-    
-    date: { type: Date }
+    humyExt1: { type: Number },
+    tempInt1Max: { type: Number},
+    tempExt1Max: { type: Number },
+    humyInt1Max: { type: Number },
+    humyExt1Max: { type: Number },
+    tempInt1Min: { type: Number},
+    tempExt1Min: { type: Number },
+    humyInt1Min: { type: Number },
+    humyExt1Min: { type: Number },
+    upperLimit1: { type: Number},
+    upperLimit2: { type: Number},
+    lowerLimit1: { type: Number },
+    lowerLimit2: { type: Number },
+    rainAmount: { type: Number },
+    rainDuration: { type: Number },
+    date: { type: Date, required:true }
 },
 
     {
