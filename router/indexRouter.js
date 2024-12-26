cc09df9715c48587277d8887eb8c1e04d9acfdca
GuillePfarr <guillepfarr@hotmail.com>
2024-12-26 11:57:57 -0300

    actualizo indexRouter.js

--- router/indexRouter.js
+++ router/indexRouter.js
@@ -5,6 +5,7 @@ import citiesRouter from './citiesRouter.js';
 import itinerariesRouter from './itinerariesRouter.js';
 import authRouter from './authRouter.js';
 import vanguardRouter from './vanguardRouter.js';
+import parameterRouter from './parameterRouter.js';
 
 const indexRouter = Router()
 const {getAllCities, getOneCitie} = citiesController
@@ -23,5 +24,7 @@ indexRouter.use('/itineraries' , itinerariesRouter)
 indexRouter.use('/auth', authRouter)
 
 indexRouter.use('/vanguard', vanguardRouter)
+
+indexRouter.use('/parameter', parameterRouter)
 
 export default indexRouter;
