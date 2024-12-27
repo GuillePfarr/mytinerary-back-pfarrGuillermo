a0b994c8713e8034ac4ae9114a2508b18817aee3
GuillePfarr <guillepfarr@hotmail.com>
2024-12-27 09:43:55 -0300

    Correct error ajusteRouter

--- router/ajusteRouter.js
+++ router/ajusteRouter.js
@@ -3,10 +3,10 @@ import ajusteController from '../controllers/ajusteController.js';
 const ajusteRouter = Router()
 const {getAllAjuste, getOneAjuste, createOneAjuste, updateOneAjuste, deleteOneAjuste} = ajusteController
 
-vanguardRouter.get('/', getAllAjuste)
-vanguardRouter.post('/', createOneAjuste)
-vanguardRouter.get('/:id', getOneAjuste)
-vanguardRouter.put('/:id', updateOneAjuste)
-vanguardRouter.delete('/:id', deleteOneAjuste)
+ajusteRouter.get('/', getAllAjuste)
+ajusteRouter.post('/', createOneAjuste)
+ajusteRouter.get('/:id', getOneAjuste)
+ajusteRouter.put('/:id', updateOneAjuste)
+ajusteRouter.delete('/:id', deleteOneAjuste)
 
 export default ajusteRouter
