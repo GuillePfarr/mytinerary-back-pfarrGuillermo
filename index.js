fec5b29f0683493288d4c1c36e47f1fe7712a769
GuillePfarr <guillepfarr@hotmail.com>
2026-04-28 19:07:23 -0300

    Add basic security headers and CORS hardening

--- index.js
+++ index.js
@@ -1,75 +1,47 @@
-// import 'dotenv/config.js'
-// import express from "express"
-// import indexRouter from "./router/indexRouter.js"
-// import cors from 'cors'
-// import './config/database.js'
-// const server = express()
-
-// server.use(cors())
-// server.use(express.json())
-// server.use('/api', indexRouter)
-// server.get('/', (request, response, next) =>{
-// response.send('Bienvenido a mi servidor ')
-// })
-// server.listen(process.env['PORT'], ()=>{console.log('Servidor corriendo en puerto ' + process.env['PORT'])})
-// import "dotenv/config.js";
-// import express from "express";
-// import indexRouter from "./router/indexRouter.js";
-// import cors from "cors";
-// import "./config/database.js";
-// import { createMqttClient } from "./config/mqtt.js";
-
-// const server = express();
-
-// server.use(cors());
-// server.use(express.json());
-
-// // MQTT client disponible para routes/controllers
-// const mqttClient = createMqttClient();
-// server.set("mqttClient", mqttClient);
-
-// server.use("/api", indexRouter);
-
-// server.get("/", (request, response) => {
-//   response.send("Bienvenido a mi servidor ");
-// });
-
-// server.listen(process.env["PORT"], () => {
-//   console.log("Servidor corriendo en puerto " + process.env["PORT"]);
-// });
-
 import "dotenv/config.js";
 import express from "express";
-import indexRouter from "./router/indexRouter.js";
 import cors from "cors";
+import helmet from "helmet";
+
+import indexRouter from "./router/indexRouter.js";
 import "./config/database.js";
 import { createMqttClient } from "./config/mqtt.js";
 
 const server = express();
 
-// ⬇️ ACÁ está el cambio
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://mytinerary-guille-pfarr.vercel.app",
+];
+
+server.use(helmet());
+
 server.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "https://mytinerary-guille-pfarr.vercel.app",
-    ],
+    origin(origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("CORS: origin not allowed"));
+    },
     credentials: false,
-  })
+  }),
 );
 
-server.use(express.json());
+server.use(express.json({ limit: "100kb" }));
 
-// MQTT client disponible para routes/controllers
 const mqttClient = createMqttClient();
 server.set("mqttClient", mqttClient);
 
 server.use("/api", indexRouter);
 
 server.get("/", (request, response) => {
-  response.send("Bienvenido a mi servidor ");
+  response.send("Bienvenido a mi servidor");
 });
 
-server.listen(process.env["PORT"], () => {
-  console.log("Servidor corriendo en puerto " + process.env["PORT"]);
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto " + PORT);
 });
